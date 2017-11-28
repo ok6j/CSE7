582b0730dc55748c2d6b249ee3dc8f5b5edce6c5
ok6j <31050508+ok6j@users.noreply.github.com>
2017-11-28 15:41:19 -0800

    11.28.17
    11.28.17

--- Notes.py
+++ Notes.py
@@ -1,6 +1,6 @@
 print("Hello World")
 
-# Christian
+# # Christian
 
 print(3 + 5)
 print(5 - 3)
@@ -12,22 +12,47 @@ print()   # Creates a blank line
 print("See if you can figure this out")
 print(100 % 12)
 
-# Variables
+# # Variables
 car_name ="Wiebe Mobile"
 car_type = "Lamborghini Sesto Elemento"
 car_cylinders = 8
 car_mpg = 9000.1
 
-# Inline Printing
+# # Inline Printing
 print("My car is the %s." % car_name)
 print("My car is the %s. It is a %s" % (car_name, car_type))
 
-# Taking Input
+# # Taking Input
 name = input("What is your name? ")
 print("Hello %s." % name)
-print(name)
 
-# Age#
+# print(name)
+#
+# # Age
 age = input("How old are you? ")
 print("Oh you're %s." % age)
-print(age)
+
+# Change to the file
+
+def print_hw():
+    print("Hello World")
+
+
+print_hw()
+
+
+def say_hi(name):  # name is a "parameter"
+    print("Hello %s" % name)
+    print("I hope you have a fantastic day.")
+
+
+say_hi("John")
+
+
+def birthday(age):
+    age += 1   # age = age + 1
+    print(age)
+
+say_hi("John")
+print("John is 15. Next year:")
+birthday(15)
