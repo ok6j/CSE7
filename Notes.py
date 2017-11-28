8f0a4fc396e82b06ea479a0f2fb0772c75f97d35
ok6j <31050508+ok6j@users.noreply.github.com>
2017-11-28 15:07:16 -0800

    Adding Previous Notes
    11.27.17

---
+++ Notes.py
@@ -0,0 +1,33 @@
+print("Hello World")
+
+# Christian
+
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(6 / 2)
+print(3 ** 2)
+
+print()   # Creates a blank line
+print("See if you can figure this out")
+print(100 % 12)
+
+# Variables
+car_name ="Wiebe Mobile"
+car_type = "Lamborghini Sesto Elemento"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+
+# Taking Input
+name = input("What is your name? ")
+print("Hello %s." % name)
+print(name)
+
+# Age#
+age = input("How old are you? ")
+print("Oh you're %s." % age)
+print(age)
