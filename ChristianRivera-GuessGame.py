413a7d12cd225f75080de1869a224334073f1141
ok6j <31050508+ok6j@users.noreply.github.com>
2017-12-04 15:39:40 -0800

    12.4.17
    12.4.17

--- ChristianRivera-GuessGame.py
+++ ChristianRivera-GuessGame.py
@@ -13,3 +13,10 @@ def number(guess):
 
 
 
+
+#Generate a random number between one and 50
+#Get a number (input) from the user
+#Compare the number to input  #Operators and Recasters say true if its the same say false if not
+#add "higher or "lower" operators
+#add 5 guesses
+#make a variable to count the number of guesses says 5 guesses goes down
---
+++ Warmup.py
@@ -0,0 +1,25 @@
+def reverse_order(first_name, last_name):
+    # print("%s %s" % (last_name, first_name))
+    print("%s %s" % (last_name, first_name))  # Concatenation
+
+
+def reverse_order():
+    first_name = input('What is your first name?')
+    last_name = input('What is your last name?')
+    print("%s %s" % (last_name, first_name))
+
+
+"""Warmup #2
+Write a function called "happy_bday" 
+that "sings" (prints)
+the Happy Birthday song
+
+It must take one parameter called "name"
+"""
+
+def happy_bday(name):
+    print("Happy birthday to you")
+    print("Happy birthday to you.")
+    print("Happy birthday dear " % name)
+    print("Happy birthday to you!")
+def
