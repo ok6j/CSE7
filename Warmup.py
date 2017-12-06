f3f65cd539da932f2c9e09805e62e9939ea0abef
ok6j <31050508+ok6j@users.noreply.github.com>
2017-12-06 15:07:44 -0800

    Reconcile Issues

--- Warmup.py
+++ Warmup.py
@@ -27,3 +27,10 @@ def happy_bday(name):
 def add_py(name):
     print("%s .py % name")  # Solution 1
     print(name + ".py")  # Solution 2
+
+
+def add(num1, num2, num3):
+    print(num1, + num2 + num3)
+
+
+add(90, 900, 9000)
