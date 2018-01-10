e20c861f79e499f2d690769a4f5f9bd075a68384
ok6j <31050508+ok6j@users.noreply.github.com>
2018-01-10 15:40:23 -0800

    1.10.18
    1.10.18

--- Lucky 7s.py
+++ Lucky 7s.py
@@ -3,9 +3,10 @@ dice1 = (random.randint(1, 6))
 
 dice2 = (random.randint(1, 6))
 
-total = dice1 + dice2
-print("You rolled a", total)
 money = 15
 
+while money > 0:
+    total = dice1 + dice2
+    print("You rolled a", total)
 
-if
+
