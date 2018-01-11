25831610833fe26f163977a64547508d927003e4
ok6j <31050508+ok6j@users.noreply.github.com>
2018-01-11 15:39:35 -0800

    1.11.18
    1.11.18

--- Lucky 7s.py
+++ Lucky 7s.py
@@ -1,12 +1,16 @@
 import random
-dice1 = (random.randint(1, 6))
-
-dice2 = (random.randint(1, 6))
 
 money = 15
-
+rounds = 0
 while money > 0:
+    dice1 = (random.randint(1, 6))
+    dice2 = (random.randint(1, 6))
     total = dice1 + dice2
     print("You rolled a", total)
-
-
+    print("You now have %s dollars" % money)
+    print("Round: %s" % rounds)
+    if total == 7:
+        money += 4
+    else:
+        money -= 1
+        rounds += 1
